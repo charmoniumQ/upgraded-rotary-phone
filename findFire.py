7d2061e64d37cd3231062f756ec7bc2e92408143
lzuehsow <lydiazuehsow@gmail.com>
2019-03-02 16:52:32 -0500

    Pushing calibrated code

--- findFire.py
+++ findFire.py
@@ -7,11 +7,9 @@ class findFire(object):
 	def __init__(self):
 		self.cap = cv2.VideoCapture(1)
 		self.flag = 0
+		self.min_area = 10
 		self.fire_coords = []
 		self.fire_cnts = []
-
-	def nothing(*arg):
-		pass
 
 	def getLight(self, frame):
 		self.fire_coords = []
@@ -22,40 +20,14 @@ class findFire(object):
 		thresh = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY)[1]
 		thresh = cv2.erode(thresh, None, iterations=2)
 		thresh = cv2.dilate(thresh, None, iterations=4)
-		# im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-
-		# for contour in contours:
-		# 	M = cv.moments(contour)
-		# 	area = cv.contourArea(contour)
-		# 	if area >= self.min_area:
-		# 		cx = int(M['m10']/M['m00'])
-		# 		cy = int(M['m01']/M['m00'])
-		# 		self.fire_coords.append((cx, cy))
-		# 		self.fire_cnts.append(contour)
 
 		return thresh
 
 	def getColor(self, frame, val):
-		# inverted = cv2.bitwise_not(frame)
-
-		# lowHue = cv2.getTrackbarPos('lowHue', 'colorTest')
-		# highHue = cv2.getTrackbarPos('highHue', 'colorTest')
-
 		hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 		blurred = cv2.GaussianBlur(hsv, (11, 11), 0)
-		mask = cv2.inRange(hsv, (10, 120,220), (100, 255,255))
+		mask = cv2.inRange(hsv, (0, 0, 245), (42, 120, 255))
 		thresh = cv2.erode(mask, None, iterations=10)
-		# thresh = cv2.dilate(mask, None, iterations=2)
-		# im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-
-		# for contour in contours:
-		# 	M = cv.moments(contour)
-		# 	area = cv.contourArea(contour)
-		# 	if area >= self.min_area:
-		# 		cx = int(M['m10']/M['m00'])
-		# 		cy = int(M['m01']/M['m00'])
-		# 		self.fire_coords.append((cx, cy))
-		# 		self.fire_cnts.append(contour)
 
 		return mask
 
@@ -64,14 +36,21 @@ class findFire(object):
 		light = self.getLight(frame)
 		color = self.getColor(frame, val)
 
-		return color
+		output = cv2.bitwise_and(light, color)
+		im2, contours, hierarchy = cv2.findContours(output,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+		for contour in contours:
+			M = cv2.moments(contour)
+			area = cv2.contourArea(contour)
+			if area >= self.min_area:
+				cx = int(M['m10']/M['m00'])
+				cy = int(M['m01']/M['m00'])
+				self.fire_coords.append((cx, cy))
+				self.fire_cnts.append(contour)
+
+		return output
 
 	def run(self):
-		# cv2.namedWindow('colorTest', cv2.WINDOW_NORMAL)
-		# icol = (0, 100, 80, 10, 255, 255)   # Red
-		# cv2.createTrackbar('Hue_Low','colorTest',icol[0], 255, self.nothing)
-		# cv2.createTrackbar('Hue_High','colorTest',icol[3], 255, self.nothing)
-
 		while(cv2.waitKey(1) != 27):
 			ret, frame = ff.cap.read() #Capture frame-by-frame
 			if not ret: #Invalid
@@ -80,8 +59,7 @@ class findFire(object):
 
 			proc = self.process_image(frame)
 			cv2.imshow('proc', proc) #Display the resulting frame
-			# if cv2.waitKey(1) & 0xFF == ord('q'):
-			# 	break
+			# self.
 
 		#release the capture
 		temp = cap.release() #if frame read correctly == True
